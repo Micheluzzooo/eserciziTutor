fedfcc38eceff5bcc0316960b41ad3b523ff63dc
Micheluzzooo <156191514+Micheluzzooo@users.noreply.github.com>
2024-01-18 16:06:31 +0100

    Add files via upload

---
+++ Gli oggetti, il costruttore, getter e setter - 1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Studente studente = new Studente("Mario", "Rossi", 65338);
+        Studente studente2 = new Studente("Luca", "Bianchi", 64299);
+        System.out.println("Studente: " + studente);
+        System.out.println("Studente: " + studente2);
+    }
+}
