fb5e3c949af676fc2af49f0665c3428549954e66
Micheluzzooo <156191514+Micheluzzooo@users.noreply.github.com>
2024-01-18 16:51:09 +0100

    Delete Gli oggetti, il costruttore, getter e setter - 1 directory

--- Gli oggetti, il costruttore, getter e setter - 1/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        Studente studente = new Studente("Mario", "Rossi", 65338);
-        Studente studente2 = new Studente("Luca", "Bianchi", 64299);
-        System.out.println("Studente: " + studente);
-        System.out.println("Studente: " + studente2);
-    }
-}
