356f0998bcfcdef0f2bbb0c1bf9494d1e66a0fd6
Micheluzzooo <156191514+Micheluzzooo@users.noreply.github.com>
2024-02-01 17:20:25 +0100

    Add files via upload

---
+++ HashSet3/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+/*
+    Scrivere una funzione che restituisca un hashset riempito
+    Creare un oggeto dello stesso tipo inserito nell'HashSet e popolarlo
+    Scorrere il set, per ogni elemento verificare se è uguale all'oggetto creato ed eliminarlo
+    Svuotare l'hashset, verificarlo e stampare il risultato
+
+*/
+public class Main {
+    public static void main(String[] args) {
+
+        HashSet<String> carSet = cars();
+        String car = "Audi";
+        for(int i = 0; i < cars().size(); i++) {
+            if(carSet.contains(car)) {
+                carSet.remove(car);
+            }
+        }
+        System.out.println(carSet);
+
+        HashSet carSet2 = cars();
+        String car1 = "Opel";
+        Iterator<String> iterator =  carSet2.iterator();
+        while(iterator.hasNext()) {
+            String element = iterator.next();
+            //System.out.println(element);
+            if(element.equals(car1)) {
+                carSet2.remove(car1);
+            }
+        }
+        System.out.println(carSet2);
+    }
+
+    public static HashSet<String> cars() {
+        HashSet<String> cars = new HashSet<>();
+        cars.add("Audi");
+        cars.add("BMW");
+        cars.add("VW");
+        cars.add("Opel");
+        cars.add("Fiat");
+        cars.add("Mercedes");
+        return cars;
+    }
+}
