a989d1f8a9a04156fed01221fe7effa44909fad2
Micheluzzooo <156191514+Micheluzzooo@users.noreply.github.com>
2024-02-13 16:32:13 +0100

    Add files via upload

---
+++ Date3/src/Main.java
@@ -0,0 +1,23 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class Main {
+    public static void main(String[] args) {
+        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        System.out.println("Data: " + data);
+
+        String printYear = String.valueOf(data.getYear());
+        System.out.println("Year: " + printYear);
+
+        String printMonth = String.valueOf(data.getMonth());
+        System.out.println("Year: " + printMonth);
+
+        String printDay = String.valueOf(data.getDayOfMonth());
+        System.out.println("Year: " + printDay);
+
+        String printDayOfWeek = String.valueOf(data.getDayOfWeek());
+        System.out.println("Year: " + printDayOfWeek);
+
+    }
+}
