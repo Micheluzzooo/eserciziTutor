1d59a83bf566ae8c5796513d73ed1550ea73ccdc
Micheluzzooo <156191514+Micheluzzooo@users.noreply.github.com>
2024-04-11 17:34:07 +0200

    Add files via upload

---
+++ esercizioCors/src/test/java/com/example/esercizioCors/EsercizioCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esercizioCors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioCorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
