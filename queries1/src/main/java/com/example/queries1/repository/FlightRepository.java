51a74c279af857d771b9a5b8fae96787965a5654
Micheluzzooo <156191514+Micheluzzooo@users.noreply.github.com>
2024-05-07 20:45:43 +0200

    Update FlightRepository.java

--- queries1/src/main/java/com/example/queries1/repository/FlightRepository.java
+++ queries1/src/main/java/com/example/queries1/repository/FlightRepository.java
@@ -3,5 +3,11 @@ package com.example.queries1.repository;
 import com.example.queries1.entity.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+    @Modifying
+    @Query(value = "INSERT INTO Flight (description, from_airport, to_airport, status) VALUES (:description, :fromAirport, :toAirport, :status)", nativeQuery = true)
+    void saveFlight(@Param("description") String description, @Param("fromAirport") String fromAirport, @Param("toAirport") String toAirport, @Param("status") Flight.Status status);
+
 }
