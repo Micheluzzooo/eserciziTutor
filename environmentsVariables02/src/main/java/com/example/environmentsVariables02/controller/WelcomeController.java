73b2b48a279d34ecf590f59f49b0ac7be9e47901
Micheluzzooo <156191514+Micheluzzooo@users.noreply.github.com>
2024-05-08 18:13:22 +0200

    Update WelcomeController.java

--- environmentsVariables02/src/main/java/com/example/environmentsVariables02/controller/WelcomeController.java
+++ environmentsVariables02/src/main/java/com/example/environmentsVariables02/controller/WelcomeController.java
@@ -1,25 +1,18 @@
 package com.example.environmentsVariables02.controller;
 
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Profile;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@ConfigurationProperties
-@Profile("prod")
 @RestController
-@RequestMapping
 public class WelcomeController {
 
+    @Value("${welcomeMsg}")
     private String welcomeMsg;
 
-    public void setWelcomeMsg(String welcomeMsg) {
-        this.welcomeMsg = welcomeMsg;
-    }
-
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String getWelcomeMessage() {
         return welcomeMsg;
     }
