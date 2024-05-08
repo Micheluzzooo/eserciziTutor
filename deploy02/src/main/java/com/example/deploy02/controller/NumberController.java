f0a06fd2d16e5701894386584aaf1c525347c545
Micheluzzooo <156191514+Micheluzzooo@users.noreply.github.com>
2024-05-08 16:08:49 +0200

    Add files via upload

---
+++ deploy02/src/main/java/com/example/deploy02/controller/NumberController.java
@@ -0,0 +1,21 @@
+package com.example.deploy02.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+@RequestMapping("/number")
+public class NumberController {
+
+    @GetMapping("/sum")
+    public int getSum() {
+        Random random = new Random();
+        int num1 = random.nextInt(100);
+        int num2 = random.nextInt(100);
+        return num1 + num2;
+    }
+}
