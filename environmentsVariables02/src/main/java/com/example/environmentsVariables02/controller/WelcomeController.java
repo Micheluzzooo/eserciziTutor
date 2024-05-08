c8a53f13896d3c0fda25e99f1be3a03ce420b19e
Micheluzzooo <156191514+Micheluzzooo@users.noreply.github.com>
2024-05-08 18:18:37 +0200

    Update WelcomeController.java

--- environmentsVariables02/src/main/java/com/example/environmentsVariables02/controller/WelcomeController.java
+++ environmentsVariables02/src/main/java/com/example/environmentsVariables02/controller/WelcomeController.java
@@ -1,7 +1,5 @@
 package com.example.environmentsVariables02.controller;
 
-
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
